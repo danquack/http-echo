d16770b43394ca87de5511441017d8ad02b981a7
Daniel Quackenbush <25692880+danquack@users.noreply.github.com>
2022-06-29 16:44:27 -0400

    write headers as well

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
 func echo(w http.ResponseWriter, req *http.Request) {
+	if b, err := json.Marshal(req.Header); err == nil {
+		fmt.Printf("{\"Headers\": %v}\n", string(b))
+	}
 	if b, err := io.ReadAll(req.Body); err == nil {
 		fmt.Println(string(b))
 	}
